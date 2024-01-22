e02ef87b2ded82842abe369e22f1811383c73ff4
Ignacio Sachetto <nachosachetto1998@hotmail.com>
2024-01-21 23:25:38 -0300

    entregaN°1Sachetto

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <Text style={styles.text}>Hola, Coder!</Text>
+    <Text style={styles.textAditional}>Ignacio Sachetto</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: "yellow",
+  },
+  textAditional: {
+    color: "white",
+  }
+});
